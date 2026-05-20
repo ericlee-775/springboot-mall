e5600092b8d2a25de411fffab9d41033267e9524
Ray Ray <ericlee20030829@gmail.com>
2026-05-20 21:20:56 +0800

    1

---
+++ src/main/java/com/example/demo/Testspring2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testspring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testspring2Application.class, args);
+	}
+
+}
